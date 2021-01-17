dd8368fcb7fd0b1a1f6401ecc5864b67398ab88b
Saman Hosseini <bounoable@gmail.com>
2021-01-17 16:11:50 +0100

    feat(event): add `version` package

---
+++ event/version/constraint.go
@@ -0,0 +1,178 @@
+// Package version provides version constraining for queries.
+package version
+
+// Constraints provides the different constraints for querying events. An
+// event.Store that uses Constraints should combine the different types of
+// constraints with a logical "AND" and the different values for a constraint
+// with a logical "OR".
+type Constraints interface {
+	// Exact returns the exact versions to query for.
+	Exact() []int
+
+	// Ranges returns the version ranges to query for.
+	Ranges() []Range
+
+	// Min returns the minimum allowed versions to query for.
+	Min() []int
+
+	// Max returns the maximu allowed versions to query for.
+	Max() []int
+}
+
+// A Constraint is an option for constraints.
+type Constraint func(*constraints)
+
+// Range is a version range. Range{10, 31} would include versions 10 to 30.
+type Range [2]int
+
+type constraints struct {
+	exact  []int
+	ranges []Range
+	min    []int
+	max    []int
+}
+
+// Filter returns Constraints from the given Constraint opts.
+func Filter(opts ...Constraint) Constraints {
+	var c constraints
+	for _, opt := range opts {
+		opt(&c)
+	}
+	return c
+}
+
+// Exact returns a Constraint that only allows the exact versions v.
+func Exact(v ...int) Constraint {
+	return func(c *constraints) {
+		c.exact = append(c.exact, v...)
+	}
+}
+
+// InRange returns a Constraint that only allows versions in the Ranges r.
+func InRange(r ...Range) Constraint {
+	return func(c *constraints) {
+		c.ranges = append(c.ranges, r...)
+	}
+}
+
+// Min returns a Constraint that only allows versions that are at least one of v.
+func Min(v ...int) Constraint {
+	return func(c *constraints) {
+		c.min = append(c.min, v...)
+	}
+}
+
+// Max returns a Constraint that only allows versions that are at most one of v.
+func Max(v ...int) Constraint {
+	return func(c *constraints) {
+		c.max = append(c.max, v...)
+	}
+}
+
+// Includes determines if the Constraints c include all versions v.
+func Includes(c Constraints, v ...int) bool {
+	if violatesExact(c.Exact(), v...) {
+		return false
+	}
+
+	if violatesRanges(c.Ranges(), v...) {
+		return false
+	}
+
+	if violatesMin(c.Min(), v...) {
+		return false
+	}
+
+	if violatesMax(c.Max(), v...) {
+		return false
+	}
+
+	return true
+}
+
+func violatesExact(exact []int, vs ...int) bool {
+	if len(exact) == 0 {
+		return false
+	}
+	for _, v := range vs {
+		for _, v2 := range exact {
+			if v == v2 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func violatesRanges(ranges []Range, vs ...int) bool {
+	if len(ranges) == 0 {
+		return false
+	}
+	for _, v := range vs {
+		for _, r := range ranges {
+			if r.Includes(v) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func violatesMin(min []int, vs ...int) bool {
+	if len(min) == 0 {
+		return false
+	}
+	for _, v := range vs {
+		for _, m := range min {
+			if v >= m {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func violatesMax(max []int, vs ...int) bool {
+	if len(max) == 0 {
+		return false
+	}
+	for _, v := range vs {
+		for _, m := range max {
+			if v <= m {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (c constraints) Exact() []int {
+	return c.exact
+}
+
+func (c constraints) Ranges() []Range {
+	return c.ranges
+}
+
+func (c constraints) Min() []int {
+	return c.min
+}
+
+func (c constraints) Max() []int {
+	return c.max
+}
+
+// Start returns the inclusive beginning of the Range (r[0]).
+func (r Range) Start() int {
+	return r[0]
+}
+
+// End returns the exclusive end of the Range (r[1]).
+func (r Range) End() int {
+	return r[1]
+}
+
+// Includes returns true if v is within the Range r.
+func (r Range) Includes(v int) bool {
+	return v >= r[0] && v < r[1]
+}
---
+++ event/version/constraint_test.go
@@ -0,0 +1,146 @@
+package version_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/modernice/goes/event/version"
+)
+
+func TestExact(t *testing.T) {
+	v := []int{1, 3, 6}
+	c := version.Filter(version.Exact(v...))
+
+	if !reflect.DeepEqual(c.Exact(), v) {
+		t.Fatalf("c.Exact() should return %v; got %v", v, c.Exact())
+	}
+
+	tests := map[int]bool{
+		1:    true,
+		3:    true,
+		6:    true,
+		0:    false,
+		2:    false,
+		4:    false,
+		5:    false,
+		7:    false,
+		100:  false,
+		-100: false,
+	}
+
+	runTests(t, c, tests)
+}
+
+func TestInRange(t *testing.T) {
+	rs := []version.Range{
+		{2, 18},
+		{22, 31},
+		{28, 40},
+	}
+
+	c := version.Filter(version.InRange(rs...))
+
+	if !reflect.DeepEqual(c.Ranges(), rs) {
+		t.Fatalf("c.Ranges() should return %v; got %v", rs, c.Ranges())
+	}
+
+	tests := map[int]bool{
+		0:  false,
+		1:  false,
+		2:  true,
+		10: true,
+		17: true,
+		18: false,
+		21: false,
+		22: true,
+		28: true,
+		30: true,
+		31: true,
+		39: true,
+		40: false,
+	}
+
+	runTests(t, c, tests)
+}
+
+func TestMin(t *testing.T) {
+	min := []int{3, 7, 10}
+	c := version.Filter(version.Min(min...))
+
+	if !reflect.DeepEqual(c.Min(), min) {
+		t.Fatalf("c.Min() should return %v; got %v", min, c.Min())
+	}
+
+	tests := map[int]bool{
+		0:   false,
+		1:   false,
+		2:   false,
+		3:   true,
+		5:   true,
+		7:   true,
+		100: true,
+	}
+
+	runTests(t, c, tests)
+}
+
+func TestMax(t *testing.T) {
+	max := []int{30, 70, 100}
+	c := version.Filter(version.Max(max...))
+
+	if !reflect.DeepEqual(c.Max(), max) {
+		t.Fatalf("c.Max() should return %v; got %v", max, c.Max())
+	}
+
+	tests := map[int]bool{
+		0:   true,
+		1:   true,
+		2:   true,
+		3:   true,
+		5:   true,
+		7:   true,
+		29:  true,
+		30:  true,
+		31:  true,
+		100: true,
+		101: false,
+		200: false,
+	}
+
+	runTests(t, c, tests)
+}
+
+func TestFilter(t *testing.T) {
+	c := version.Filter(
+		version.Min(3),
+		version.Max(80),
+		version.InRange(
+			version.Range{20, 31},
+			version.Range{100, 111},
+		),
+	)
+
+	tests := map[int]bool{
+		0:   false,
+		2:   false,
+		3:   false,
+		19:  false,
+		20:  true,
+		25:  true,
+		30:  true,
+		31:  false,
+		90:  false,
+		100: false,
+		110: false,
+	}
+
+	runTests(t, c, tests)
+}
+
+func runTests(t *testing.T, c version.Constraints, tests map[int]bool) {
+	for give, want := range tests {
+		if got := version.Includes(c, give); got != want {
+			t.Errorf("expected c.Includes(%d) to return %t; got %t", give, want, got)
+		}
+	}
+}
