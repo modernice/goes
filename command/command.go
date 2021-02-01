0357e24aabb4ed403e961c9d4fe42664bb16fa33
Saman Hosseini <bounoable@gmail.com>
2021-02-01 17:22:49 +0100

    feat(command): define api

---
+++ command/command.go
@@ -0,0 +1,110 @@
+package command
+
+//go:generate mockgen -source=command.go -destination=./mocks/command.go
+
+import (
+	"context"
+	"io"
+
+	"github.com/google/uuid"
+)
+
+// A Command is a (domain) command that can be dispatched through a Bus.
+type Command interface {
+    // ID returns the Command ID.
+	ID() uuid.UUID
+
+    // Name returns the Command name.
+	Name() string
+
+    // Payload returns the Command Payload.
+	Payload() Payload
+
+    // AggregateName returns the Aggregates name the Command belongs to.
+    // (optional)
+    AggregateName() string
+
+    // AggregateID returns the Aggregates UUID the Command belongs to.
+    // (optional)
+    AggregateID() uuid.UUID
+}
+
+// Payload is the payload for a Command.
+type Payload interface{}
+
+// An Encoder encodes and decodes Payloads.
+type Encoder interface {
+	// Encode encodes the given Payload and writes the result into the Writer.
+	Encode(io.Writer, Payload) error
+
+	// Decode decodes the Payload in Reader r.
+	Decode(name string, r io.Reader) (Payload, error)
+}
+
+// A Bus dispatches Commands to the appropriate Handlers.
+type Bus interface {
+    // Dispatch sends the Command to the appropriate Handlers. Which Handlers
+    // receive the Command can vary and depends on the underlying Bus
+    // implementation.
+	Dispatch(context.Context, Command) error
+}
+
+// Option is a Command option.
+type Option func(*base)
+
+type base struct {
+	id      uuid.UUID
+	name    string
+	payload Payload
+    aggregateName string
+    aggregateID uuid.UUID
+}
+
+// ID returns an Option that overrides the auto-generated UUID of a Command.
+func ID(id uuid.UUID) Option {
+    return func(b *base) {
+        b.id = id
+    }
+}
+
+// Aggregate returns an Option that links a Command to an Aggregate.
+func Aggregate(name string, id uuid.UUID) Option {
+    return func(b *base) {
+        b.aggregateName = name
+        b.aggregateID = id
+    }
+}
+
+// New returns a new Command with the given name and Payload.
+func New(name string, pl Payload, opts ...Option) Command {
+    cmd := base{
+        id: uuid.New(),
+		name:    name,
+		payload: pl,
+	}
+    for _, opt := range opts {
+        opt(&cmd)
+    }
+    return &cmd
+}
+
+func (cmd *base) ID() uuid.UUID {
+	return cmd.id
+}
+
+func (cmd *base) Name() string {
+	return cmd.name
+}
+
+func (cmd *base) Payload() Payload {
+	return cmd.payload
+}
+
+func (cmd *base) AggregateName() string {
+    return cmd.aggregateName
+}
+
+func (cmd *base) AggregateID() uuid.UUID {
+    return cmd.aggregateID
+}
+
---
+++ command/command_test.go
@@ -0,0 +1,71 @@
+package command_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/modernice/goes/aggregate"
+	"github.com/modernice/goes/command"
+)
+
+type mockPayload struct {
+    A bool
+    B string
+}
+
+func TestNew(t *testing.T) {
+    pl := mockPayload{
+        A: true,
+        B: "bar",
+    }
+    cmd := command.New("foo", pl)
+
+    var _ uuid.UUID = cmd.ID()
+
+    if cmd.ID() == uuid.Nil {
+        t.Errorf("cmd.ID should return a non-zero UUID; got %s", cmd.ID())
+    }
+
+    if cmd.Name() != "foo" {
+        t.Errorf("cmd.Name should return %q; got %q", "foo", cmd.Name())
+    }
+
+    if cmd.Payload() != pl {
+        t.Errorf("cmd.Payload should return %#v; got %#v", pl, cmd.Payload())
+    }
+}
+
+func TestID(t *testing.T) {
+    id := uuid.New()
+    cmd := command.New("foo", mockPayload{}, command.ID(id))
+
+    if cmd.ID() != id {
+        t.Errorf("ID Option did not apply. want=%s got=%s", id, cmd.ID())
+    }
+}
+
+func TestAggregateName(t *testing.T) {
+    a := aggregate.New("foo", uuid.New())
+    cmd := command.New(
+        "foo",
+        mockPayload{},
+        command.Aggregate(a.AggregateName(), a.AggregateID()),
+    )
+
+    if cmd.AggregateName() != a.AggregateName() {
+        t.Fatalf(
+            "cmd.AggregateName should return %q; got %q",
+            a.AggregateName(),
+            cmd.AggregateName(),
+        )
+    }
+
+    if cmd.AggregateID() != a.AggregateID() {
+        t.Fatalf(
+            "cmd.AggregateID should return %q; got %q",
+            a.AggregateID(),
+            cmd.AggregateID(),
+        )
+    }
+}
+
