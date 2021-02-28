530f6b7d9c3f554f3b4a1257645969e8c32b52f8
Saman Hosseini <bounoable@gmail.com>
2021-02-28 15:40:40 +0100

    feat(aggregate/snapshot): define `Store` type

---
+++ aggregate/snapshot/repository.go
@@ -0,0 +1,48 @@
+package snapshot
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/modernice/goes/aggregate"
+)
+
+// Store is a database for Snapshots.
+type Store interface {
+	// Save saves the given Snapshot into the Store.
+	Save(context.Context, Snapshot) error
+
+	// Latest returns the latest Snapshot for the Aggregate with the given name
+	// and UUID.
+	Latest(context.Context, string, uuid.UUID) (Snapshot, error)
+
+	// Version returns the Snapshot with the given version for the Aggregate
+	// with the given name and UUID. Implementations should return an error if
+	// the specified Snapshot does not exist in the Store.
+	Version(context.Context, string, uuid.UUID, int) (Snapshot, error)
+
+	// Query queries the Store for Snapshots and returns a Stream for those
+	// Snapshots.
+	Query(context.Context, aggregate.Query) (Stream, error)
+
+	// Delete deletes a Snapshot from the Store.
+	Delete(context.Context, Snapshot) error
+}
+
+// A Stream iterates over Snapshots.
+type Stream interface {
+	// Next fetches the next Snapshot from the Stream and returns whether the
+	// fetch was successful. When Next returns false, Err should return the
+	// error that made the fetch fail. Otherwise Snapshot returns the current
+	// Snapshot.
+	Next(context.Context) bool
+
+	// Snapshot returns the current Snapshot from the Stream.
+	Snapshot() Snapshot
+
+	// Err returns the current error from the Stream.
+	Err() error
+
+	// Close closes the Stream.
+	Close(context.Context) error
+}
