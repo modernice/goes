847fc9bf3e38785c2175facbaa4afd19ac5819dd
Saman Hosseini <bounoable@gmail.com>
2021-11-25 21:50:38 +0100

    docs(projection): add docs

--- projection/trigger.go
+++ projection/trigger.go
@@ -9,8 +9,14 @@ type TriggerOption func(*Trigger)
 
 // A Trigger is used by Schedules to trigger a Job.
 type Trigger struct {
-	Reset  bool
-	Query  event.Query
+	// Reset projections before applying events.
+	Reset bool
+
+	// Override the Query that is used to query events from the event store.
+	Query event.Query
+
+	// Additional filters that are applied in-memory to the query result from
+	// the event store.
 	Filter []event.Query
 }
 
@@ -23,28 +29,28 @@ func NewTrigger(opts ...TriggerOption) Trigger {
 	return t
 }
 
-// Reset returns a TriggerOption that resets Projections before applying Events
-// onto them. Resetting a Projection is done by first resetting the progress of
-// the Projection (if it implements progressor). Then, if the Projection has a
-// Reset method, that method is called to allow for custom reset logic.
+// Reset returns a TriggerOption that resets projections before applying events
+// onto them. Resetting a projection is done by first resetting the progress of
+// the projection (if it implements progressor). Then, if the projection has a
+// `Reset()` method, that method is called to allow for custom reset logic.
 func Reset() TriggerOption {
 	return func(t *Trigger) {
 		t.Reset = true
 	}
 }
 
-// Query returns a TriggerOption that sets the Query of the Trigger.
+// Query returns a TriggerOption that sets the Query of a Trigger.
 //
 // When a Job is created by a Schedule, it is passed an event query to fetch the
-// events for the Projections. By default, this query fetches the events
-// configured in the Schedule sorted by time. A custom query may be provided
-// using the Query option. Don't forget to configure correct sorting when
-// providing a custom query:
+// events for the projections. By default, this query fetches all events that
+// are configured in the triggered Schedule, sorted by time. A custom query may
+// be provided using the `Query` option. Don't forget to configure correct
+// sorting when providing a custom query:
 //
 //	var s projection.Schedule
 //	err := s.Trigger(context.TODO(), projection.Query(query.New(
 //		query.AggregateName("foo", "bar"),
-//		query.SortBy(event.SortTime, event.SortAsc),
+//		query.SortBy(event.SortTime, event.SortAsc), // to ensure correct sorting
 //	)))
 func Query(q event.Query) TriggerOption {
 	return func(t *Trigger) {
@@ -52,13 +58,12 @@ func Query(q event.Query) TriggerOption {
 	}
 }
 
-// Filter returns a TriggerOption that adds filters to the Trigger.
+// Filter returns a TriggerOption that adds filters to a Trigger.
 //
-// Queried events can be further filtered using the Filter option. Filters
-// are applied in-memory, after the events have already been fetched from
-// the event store. When multiple filters are passed, events must match
-// against every filter to be applied to the Projections. Sorting options of
-// filters are ignored.
+// Queried events can be further filtered using the `Filter` option. Filters
+// are applied in-memory, after the events have been fetched from the event
+// store. When multiple filters are configured, events must match against every
+// filter to be applied to projections. Sorting options of filters are ignored.
 //
 //	var s projection.Schedule
 //	err := s.Trigger(context.TODO(), projection.Filter(query.New(...), query.New(...)))
