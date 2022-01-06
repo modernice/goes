413f92591337eea28abc1a0f68a3ce7bf3d779f4
Saman Hosseini <bounoable@gmail.com>
2022-01-06 17:19:04 +0100

    docs(event): fix docs

--- event/extract.go
+++ event/extract.go
@@ -2,19 +2,19 @@ package event
 
 import "github.com/google/uuid"
 
-// ExtractAggregateID returns the ExtractAggregateID of the given event.
+// ExtractAggregateID returns the AggregateID of the given event.
 func ExtractAggregateID(evt Event) uuid.UUID {
 	id, _, _ := evt.Aggregate()
 	return id
 }
 
-// ExtractAggregateName returns the ExtractAggregateName of the given event.
+// ExtractAggregateName returns the AggregateName of the given event.
 func ExtractAggregateName(evt Event) string {
 	_, name, _ := evt.Aggregate()
 	return name
 }
 
-// ExtractAggregateVersion returns the ExtractAggregateVersion of the given event.
+// ExtractAggregateVersion returns the AggregateVersion of the given event.
 func ExtractAggregateVersion(evt Event) int {
 	_, _, v := evt.Aggregate()
 	return v
