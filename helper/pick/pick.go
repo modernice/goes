7170e06aa1332024ad1349308be55e49eb8e7304
Saman Hosseini <bounoable@gmail.com>
2022-01-19 01:16:08 +0100

    refactor(helper/pick): make non-generic

--- helper/pick/pick.go
+++ helper/pick/pick.go
@@ -2,24 +2,26 @@ package pick
 
 import "github.com/google/uuid"
 
+// An AggregateProvider has an Aggregate() (uuid.UUID, string, int) method.
+// aggregate.Aggrgeate and event.Event implement this interface.
 type AggregateProvider interface {
 	Aggregate() (uuid.UUID, string, int)
 }
 
 // AggregateID returns the id that is returned by p.Aggregate().
-func AggregateID[Provider AggregateProvider](p Provider) uuid.UUID {
+func AggregateID(p AggregateProvider) uuid.UUID {
 	id, _, _ := p.Aggregate()
 	return id
 }
 
 // AggregateName returns the name that is returned by p.Aggregate().
-func AggregateName[Provider AggregateProvider](p Provider) string {
+func AggregateName(p AggregateProvider) string {
 	_, name, _ := p.Aggregate()
 	return name
 }
 
 // AggregateVersion returns the version that is returned by p.Aggregate().
-func AggregateVersion[Provider AggregateProvider](p Provider) int {
+func AggregateVersion(p AggregateProvider) int {
 	_, _, version := p.Aggregate()
 	return version
 }
