7b4332a7cfadb145fc2ac4373815c77fd0cd75c0
Saman Hosseini <bounoable@gmail.com>
2023-04-21 23:02:37 +0200

    feat(docs): add script to generate documentation using jotbot
    Add a new script `docs.sh` to generate documentation using jotbot. The script
    checks for the `OPENAI_API_KEY` environment variable and exits if it is not set.
    It also checks if the `jotbot` binary is installed and exits if it is not found.
    The script then runs `jotbot` on the root directory of the project.

---
+++ scripts/docs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+ROOT=$(git rev-parse --show-toplevel)
+SCRIPTS="$ROOT/scripts"
+
+if [ -f "$ROOT/.env" ]; then
+	set -o allexport
+	source "$ROOT/dev.env"
+	set +o allexport
+fi
+
+if [ -z "$OPENAI_API_KEY" ]; then
+	echo "Missing OPENAI_API_KEY environment variable"
+	exit 1
+fi
+
+# check if jotbot is an installed binary
+if ! command -v jotbot &> /dev/null; then
+	echo "'jotbot' executable not found in PATH"
+	echo "Please install jotbot with"
+	echo "  go install github.com/modernice/jotbot/cmd/jotbot@main"
+	echo
+	echo "Then run this script again."
+	exit 1
+fi
+
+jotbot "$ROOT"
