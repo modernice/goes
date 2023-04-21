f67b9e3750d71278013a6040bd0f1cc266aa535d
Saman Hosseini <bounoable@gmail.com>
2023-04-21 23:13:55 +0200

    refactor(docs.sh): simplify dev.env file check and source command fix(docs.sh): exit with error if dev.env file is missing fix(docs.sh): exit with error if OPENAI_API_KEY environment variable is missing

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,11 +3,14 @@
 ROOT=$(git rev-parse --show-toplevel)
 SCRIPTS="$ROOT/scripts"
 
-if [ -f "$ROOT/dev.env" ]; then
-	set -o allexport
-	source "$ROOT/dev.env"
-	set +o allexport
+if [ ! -f "$ROOT/dev.env" ]; then
+	echo "Missing dev.env file."
+	exit 1
 fi
+
+set -o allexport
+source "$ROOT/dev.env"
+set +o allexport
 
 if [ -z "$OPENAI_API_KEY" ]; then
 	echo "Missing OPENAI_API_KEY environment variable"
